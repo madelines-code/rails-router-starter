c836a143ce139e5e9362106bfdf6526bc4c30de2
Madeline Armstrong <armstrong.madeline@gmail.com>
2021-12-15 10:19:26 -0700

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+u1 = User.create(email:'test@test.com', password:123456)
+
+p u1 
